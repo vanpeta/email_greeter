db9d7eaf0e1ab1390c0dc07b8a10cbaca02a31dd
Carlos Allende <carlosallendesanz@gmail.com>
2018-09-21 18:19:39 -0700

    adding express, entry point, dotenv, and gitignore and default test route. Server starting correctly

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").load();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./config/routes")(app);
+
+if (process.env.NODE_ENV === "production") {
+	// Express will serve up production assets
+	app.use(express.static("client/build"));
+	// Express will serve up index.html if it doesn't recognize the route
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
